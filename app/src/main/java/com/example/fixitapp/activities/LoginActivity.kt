6c10dc064f2956ad275a8390a3338bec1773e2b6
BustaPro <134396616+BustaPro@users.noreply.github.com>
2025-10-21 22:07:54 -0300

    conectar login al register con boton no tienes cuenta

--- app/src/main/java/com/example/fixitapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/fixitapp/activities/LoginActivity.kt
@@ -1,24 +1,21 @@
 package com.example.fixitapp.activities
 
 import android.content.Intent
-import android.widget.Button
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
 import com.example.fixitapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
         val btnGoToRegister = findViewById<Button>(R.id.btnGoToRegister)
 
         btnGoToRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
